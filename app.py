fd278b0c02e7e7dea8be5209026e36bffd088639
salimdridi <42705947+salimdridi@users.noreply.github.com>
2021-09-23 18:28:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def get_files(path):
     if we import the functions of this file into the other files then this part will not get imported.
 '''
 if __name__ == '__main__':
-    app.run(debug=True) #run the server on the localhost with default port of 5000
+    app.run(host='0.0.0.0', port=port, debug=True) #run the server on the localhost with default port of 5000
