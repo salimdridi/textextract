0548949550a870cd6a70b5b98dc455bedcf8eb58
salimdridi <42705947+salimdridi@users.noreply.github.com>
2021-09-23 18:34:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def get_files(path):
 '''
 if __name__ == '__main__':
    port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port, debug=True) #run the server on the localhost with default port of 5000
+   app.run(host='127.0.0.1', port=port, debug=True) #run the server on the localhost with default port of 5000
